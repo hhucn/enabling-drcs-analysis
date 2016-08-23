1ae6a1434437fa4f23558f81e64e2b30d054f645
Philipp Hagemeister <phihag@phihag.de>
2016-08-23 06:18:15 +0200

    add print_stats helper

---
+++ print_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import utils
+
+
+def main():
+	parser = argparse.ArgumentParser('print generic statistics')
+	args = parser.parse_args()
+
+	repo_list = utils.read_data('list')
+	for r in repo_list:
+		print('%40s - %d forks' % (r['html_url'], r['forks_count']))
+
+
+if __name__ == '__main__':
+	main()
