004f9cb1850ae3632030e96369d8832f4169fb0e
Philipp Hagemeister <phihag@phihag.de>
2016-09-20 06:33:21 +0200

    start better algorithm for graph size

--- graph.py
+++ graph.py
@@ -31,32 +31,86 @@ def calc_children(commit_dict):
 
 
 def calc_sizes_dumb(commit_dict):
-	for c in commit_dict.values():
-		visited = set()
-		to_visit = collections.deque([c['sha']])
-		while to_visit:
-			nsha = to_visit.pop()
-			n = commit_dict[nsha]
-			if nsha in visited:
-				continue
-			visited.add(nsha)
-			to_visit.extend(n['parents'])
-		c['size'] = len(visited)
+    for c in commit_dict.values():
+        visited = set()
+        to_visit = collections.deque([c['sha']])
+        while to_visit:
+            nsha = to_visit.pop()
+            n = commit_dict[nsha]
+            if nsha in visited:
+                continue
+            visited.add(nsha)
+            to_visit.extend(n['parents'])
+        c['size'] = len(visited)
 
 
 calc_sizes = calc_sizes_dumb
 
 
-# Clever impl goes here
-def cd2graph(commit_dict):
-	# Use numeric lookup
-	commit_list = list(commit_dict.values())
-	parents = []
-	sha2num = {sha: i for i, sha in enumerate(commit_dict.keys())}
+# Implementation from http://stackoverflow.com/a/35831759/35070
+def calc_sizes(commit_dict):
+    # Use numeric lookup
+    commit_list = list(commit_dict.values())
+    sha2num = {c['sha']: i for i, c in enumerate(commit_list)}
+    parents = [
+        [sha2num[p] for p in c['parents']]
+        for c in commit_list]
+    children = [
+        [sha2num[p] for p in c['children']]
+        for c in commit_list]
 
-def calc_sizes_cd(commit_dict):
-	g = cd2graph(commit_dict)
+    Vcount = len(commit_list)
 
+    def countReachable(cnum):
+        if ignored[cnum]:
+            return 0
+        visited = [False] * Vcount
 
-def calc_sizes_graph(g):
-	TODO
+        stack = collections.deque([cnum])
+        count = 0
+        while stack:
+            nnum = stack.pop()
+            if visited[nnum]:
+                continue
+
+            count += 1
+            visited[nnum] = True
+            for pnum in parents[nnum]:
+                if not visited[pnum]:
+                    stack.append(pnum)
+
+        if count * 2 >= Vcount:
+            return markAndCountAncestors(cnum, visited)
+        else:
+            return markSuccessors(visited)
+
+    def markAndCountAncestors(root, visited):
+        stack = [root]
+        visited[root] = False
+
+        count = 0
+        while stack:
+            nnum = stack.pop()
+            if visited[nnum] or ignored[nnum]:
+                continue
+
+            count += 1
+            visited[nnum] = True
+            ignored[nnum] = True
+
+            for cnum in children[nnum]:
+                if not visited[cnum] and not ignored[cnum]:
+                    stack.append(cnum)
+
+        return count
+
+    def markSuccessors(visited):
+        for i in range(Vcount):
+            if visited[i]:
+                ignored[i] = True
+
+        return 0
+
+    ignored = [False] * Vcount
+    for cnum, c in enumerate(commit_list):
+        c['size'] = countReachable(cnum)
