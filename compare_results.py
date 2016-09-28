4ecec927b84fc79eb873e7b88638dbec8f7e4106
Philipp Hagemeister <phihag@phihag.de>
2016-09-29 01:19:52 +0200

    also print first+last

--- compare_results.py
+++ compare_results.py
@@ -22,15 +22,18 @@ def eval_results(experiments):
             result_vals = [r['diff']['lines'] for r in results]
             best = results[0]
             second_str = (
-                '(%2d)   %6d' % (results[1]['param'], results[1]['diff']['lines'])
+                '(%2d) %6d' % (results[1]['param'], results[1]['diff']['lines'])
                 if len(results) > 1
-                else '%13s' % '-')
+                else '%11s' % '-')
 
-            print(' %-24s: best(%2d) %6d   second%s   avg %6d   median %6d' % (
+            print(' %-24s: best(%2d) %6d   second%s   avg %6d   median %6d   max %6d   first %6d   last %6d' % (
                 '%s[%d]' % (k, len(results)),
                 best['param'], best['diff']['lines'],
                 second_str,
-                statistics.mean(result_vals), statistics.median(result_vals)))
+                statistics.mean(result_vals), statistics.median(result_vals),
+                max(result_vals),
+                result_vals[0], result_vals[-1],
+                ))
 
 def main():
     parser = argparse.ArgumentParser(
