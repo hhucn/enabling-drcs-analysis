a04d644228cce4baf4aa9b52ee7134e56229916c
Philipp Hagemeister <phihag@phihag.de>
2016-09-29 00:50:31 +0200

    output result script

---
+++ compare_results.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import collections
+import random
+import time
+
+
+import sim
+import utils
+
+
+DIRNAME = 'compare_results'
+
+
+def eval_results(experiments):
+    for i, e in enumerate(experiments):
+        print('[%d] %s' % (i, e['repo']))
+        for k, results in sorted(e['res'].items()):
+            best = min(results, key=lambda r: r['diff']['lines'])
+            print(' %-20s: best(%s) %6d out of %3d' % (k, best['val'], best['diff']['lines'], len(results)))
+
+def main():
+    parser = argparse.ArgumentParser(
+        'Compare and display simulation results')
+    args = parser.parse_args()
+
+    experiments = utils.read_data('experiments', dirname=sim.DIRNAME)
+    eval_results(experiments)
+
+
+if __name__ == '__main__':
+    main()
