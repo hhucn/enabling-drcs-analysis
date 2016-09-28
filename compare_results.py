766c485da9637306aa2368005d52e6689dffd680
Philipp Hagemeister <phihag@phihag.de>
2016-09-29 01:14:47 +0200

    also output second-best

--- compare_results.py
+++ compare_results.py
@@ -18,11 +18,18 @@ def eval_results(experiments):
     for i, e in enumerate(experiments):
         print('[%d] %s' % (i, e['repo']))
         for k, results in sorted(e['res'].items()):
+            results = sorted(results, key=lambda r: r['diff']['lines'])
             result_vals = [r['diff']['lines'] for r in results]
-            best = min(results, key=lambda r: r['diff']['lines'])
-            print(' %-24s: best(%2d) %6d   avg %6d   median %6d' % (
+            best = results[0]
+            second_str = (
+                '(%2d)   %6d' % (results[1]['param'], results[1]['diff']['lines'])
+                if len(results) > 1
+                else '%13s' % '-')
+
+            print(' %-24s: best(%2d) %6d   second%s   avg %6d   median %6d' % (
                 '%s[%d]' % (k, len(results)),
                 best['param'], best['diff']['lines'],
+                second_str,
                 statistics.mean(result_vals), statistics.median(result_vals)))
 
 def main():
