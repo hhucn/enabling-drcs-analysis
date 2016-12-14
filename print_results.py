15921ea6cc3924380f5b9c49eb3f581374eebba2
Philipp Hagemeister <phihag@phihag.de>
2016-12-14 08:16:03 +0100

    fix debug output

--- print_results.py
+++ print_results.py
@@ -7,22 +7,26 @@ import sim
 import utils
 
 
-def eval_results(experiments):
+def eval_results(args, experiments):
     for i, e in enumerate(experiments):
+        idx = next(
+            i for i, f in enumerate(e['futures'])
+            if f['days'] == args.days)
+
         print('[%d] %s (%d heads), %s -> %s' % (
-            i, e['repo'], len(e['all_heads']), utils.datestr(e['ts']), utils.datestr(e['future_ts'])))
-        for k, results in sorted(e['res'].items()):
-            results = sorted(results, key=lambda r: r['diff']['lines'])
-            result_vals = [r['diff']['lines'] for r in results]
+            i, e['repo'], len(e['all_heads']), utils.datestr(e['ts']), utils.datestr(e['futures'][idx]['ts'])))
+        for k, all_results in sorted(e['res'].items()):
+            results = sorted(all_results, key=lambda r: r['diffs'][idx][args.diff_crit])
+            result_vals = [r['diffs'][idx][args.diff_crit] for r in results]
             best = results[0]
             second_str = (
-                '(%2d) %6d' % (results[1]['param'], results[1]['diff']['lines'])
+                '(%2d) %6d' % (results[1]['param'], results[1]['diffs'][idx][args.diff_crit])
                 if len(results) > 1
                 else '%11s' % '-')
 
             print(' %-24s: best(%2d) %6d   2best%s   avg %6d   median %6d   max %6d   first %6d   last %6d' % (
                 '%s[%d]' % (k, len(results)),
-                best['param'], best['diff']['lines'],
+                best['param'], best['diffs'][idx][args.diff_crit],
                 second_str,
                 statistics.mean(result_vals), statistics.median(result_vals),
                 max(result_vals),
@@ -33,14 +37,20 @@ def eval_results(experiments):
 def main():
     parser = argparse.ArgumentParser(
         'Compare and display simulation results')
-    parser.add_argument('-i', '--input-file', metavar='FILE', help='Read results from this file instead of the default')
+    parser.add_argument(
+        '-i', '--input-file', metavar='FILE',
+        help='Read results from this file instead of the default')
+    parser.add_argument(
+        '-d', '--diff-crit', metavar='CRIT', default='lines',
+        help='Difference criterion (lines, files, or len)')
+    parser.add_argument('--days', metavar='DAYS', help='Diff days', type=int, default=60)
     args = parser.parse_args()
 
     if args.input_file:
         experiments = utils.read_json(args.input_file)
     else:
         experiments = utils.read_data('experiments', dirname=sim.DIRNAME)
-    eval_results(experiments)
+    eval_results(args, experiments)
 
 
 if __name__ == '__main__':
